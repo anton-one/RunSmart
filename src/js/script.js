bc757c2ffd74f869e97794929cbc1f60253d3d8f
Anton <anton.podg@gmail.com>
2019-07-06 21:11:47 +0500

    8%

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 
 $(document).ready(function () {
+
+	// Скрипт карусели
 	$('.carousel__inner').slick({
 		speed: 1200,
 		adaptiveHeight: true,
@@ -15,4 +17,68 @@ $(document).ready(function () {
 			}
 		]
 	});
+	//
+
+
+	// Скрипт табов
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
+	//
+
+	// Скрипт открытия описания товара
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+			})
+		});
+	};
+
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
+	//
+
+
+	//Модальное окно
+	$('[data-modal=consultation]').on('click', function () {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+	$('.modal__close').on('click', function () {
+		$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+	});
+
+	$('.button_mini').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		})
+	});
+
+	//Маска ввода номера
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+
+	//Отправка с формы и смена модальных окон
+	$('form').submit(function (e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function () {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+
+
 });
